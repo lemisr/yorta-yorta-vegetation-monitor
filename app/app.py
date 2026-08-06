868bf73b7e73f126da09fa4774bfd69800063858
lemisr <pierre.misrai1@gmail.com>
2026-08-06 12:54:50 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -114,8 +114,8 @@ map_data = st_folium(
      m,
      width=1000,
      height=700,
-     returned_objects = ["all_drawings"],
-     clear_drawings = True
+    returned_objects = ["all_drawings"],
+    clear_drawings = True
 )
 
 
