145faddb565820698fa4cda9214a098f1cfd41b6
lemisr <pierre.misrai1@gmail.com>
2026-08-07 16:10:01 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -352,7 +352,7 @@ if map_data and map_data["all_drawings"]:
     selected_image = selected_images.median()
 
     selected_ndvi = (
-        image.select("B8")
+        selected_image.select("B8")
         .subtract(selected_image.select("B4"))
         .divide(
             selected_image.select("B8")
