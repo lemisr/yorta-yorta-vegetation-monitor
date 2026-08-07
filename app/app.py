5f39580f7546f634c02a5b24a6e16eb57939a492
lemisr <pierre.misrai1@gmail.com>
2026-08-07 14:06:58 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -358,7 +358,7 @@ if map_data and map_data["all_drawings"]:
         .clip(selected_aoi)
     )    
 
-    selected_ndvi_map = selected_ndvi.getMapld({
+    selected_ndvi_map = selected_ndvi.getMapId({
         "min":-1,
         "max":1,
         "palette":[
