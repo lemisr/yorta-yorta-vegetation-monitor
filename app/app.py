4419fea3cbe3bd5403353cdcc01503c6f9ca03d1
lemisr <pierre.misrai1@gmail.com>
2026-09-03 15:05:13 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -985,7 +985,7 @@ layer_control_css = """
 }
 .leaflet-control-zoom a {
    background-color: #2a2a2a !important;
-   color: #3a3a3a !important;
+   
    border-color: #444 !important;
 }
 .leaflet-control-zoom a:hover {
