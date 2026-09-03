d29028776ed8588dd59a449bc01790d786932c30
lemisr <pierre.misrai1@gmail.com>
2026-09-03 13:55:49 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -973,7 +973,7 @@ layer_control_css = """
    background-color: #2a2a2a !important;
    border-color: #444 !important;
 }
-.leaflet-draw-toolbar a {
+.leaflet-draw-toolbar a::before {
    filter: invert(1) brightness(1.8);
 }  
 .leaflet-draw-toolbar a:hover {
